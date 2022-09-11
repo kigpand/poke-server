036a4648b6d95115bf4fb1d07d3ef2760fe3c871
kigpand <kigpand@naver.com>
2022-09-11 13:23:53 +0900

    sql setting

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected');
+})
 
 app.get('/pokemon/:generate', (req, res) => {
     res.send(`generate: ${req.params.generate}`);
